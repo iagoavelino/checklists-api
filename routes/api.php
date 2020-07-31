6af3ffc6d5c2a698ecacc9e021888b283325ca6f
Iago Avelino <iagoatrajano@gmail.com>
2020-07-30 23:42:41 -0300

    Adding routes for user

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('API')->group(function() {
+    Route::middleware(['throttle:60'])->group(function () {
+        Route::apiResource('user', 'UserController');
+    });
 });
